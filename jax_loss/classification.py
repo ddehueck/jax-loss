19e7bce7aa32dcee744f692d0c19cfee59d6fc64
FerranAlet <ferranalet@gmail.com>
2022-06-22 00:29:26 -0400

    Small typo to fix bug in numerical stability
    One has to clip -x, not x. Otherwise, it is numerically unstable. Checked on my own data, and the link mentioned on line 7 also uses -x.
    
    This bug only was only making it less stable, not incorrect.

--- jax_loss/classification.py
+++ jax_loss/classification.py
@@ -12,7 +12,7 @@ def bce_w_logits(x, y, weight=None, average=True):
     :param average: Boolean to average resulting loss vector
     :return: Scalar value
     """
-    max_val = np.clip(x, 0, None)
+    max_val = np.clip(-x, 0, None)
     loss = x - x * y + max_val + np.log(np.exp(-max_val) + np.exp((-x - max_val)))
 
     if weight is not None:
